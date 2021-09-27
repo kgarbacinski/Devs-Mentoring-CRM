b73575e2e941041283c9e3622613d9c0bddf60e8
Rafal Garbacik <rafalgbc@gmail.com>
2021-09-27 16:24:51 +0200

    Create urls.py requirements.txt index.html Update settings.py views.py urls.py

--- Meetings_calendar/views.py
+++ Meetings_calendar/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import View
+
 
 # Create your views here.
+class Index(View):
+    template_name = 'Meetings_calendar/index.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
