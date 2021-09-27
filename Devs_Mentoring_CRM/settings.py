30b142fb2081c56373b42178844b59d5da299f25
Rafal Garbacik <rafalgbc@gmail.com>
2021-09-27 13:48:22 +0200

    Update settings.py

--- Devs_Mentoring_CRM/settings.py
+++ Devs_Mentoring_CRM/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'Account_management',
+    'Exercises_checker',
+    'Files_organizer',
+    'Meetings_calendar',
+    'Rest_API',
 ]
 
 MIDDLEWARE = [
