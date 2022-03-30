94ebb16c62c7fd10a386426eb02fb05b1b953f62
Sebastian <shadow.smoke7@gmail.com>
2022-03-30 17:22:38 +0200

    Add dockerization

--- ExercisesComputing/ExercisesComputing/urls.py
+++ ExercisesComputing/ExercisesComputing/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("REST_API.urls"))
+    path('', include("REST_API.urls")),
+    path('', include('django_prometheus.urls')),
 ]
